6896891e41749cac7847c5dce97aece1458e0914
Rahul <rktechnicalgroup69@gmail.com>
2023-12-06 18:40:59 +0530

    Initial commit date: 06/12/2023

---
+++ src/components/shared/UserCard.tsx
@@ -0,0 +1,46 @@
+import { Models } from "appwrite";
+import { Link } from "react-router-dom";
+
+import { Button } from "../ui/button";
+import { useUserContext } from "@/context/AuthContext";
+import { useGetCurrentUser } from "@/lib/react-query/queriesAndMutations";
+
+type UserCardProps = {
+  user: Models.Document;
+};
+
+const UserCard = ({ user }: UserCardProps) => {
+  const {data: currentUser} = useGetCurrentUser();
+  return (
+    <div>
+      {currentUser?.$id !== user.$id && (
+        <Link to={`/profile/${user.$id}`} className="user-card">
+          <img
+            src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+            alt="creator"
+            className="rounded-full w-14 h-14"
+          />
+
+          <div className="flex-center flex-col gap-1">
+            <p className="base-medium text-light-1 text-center line-clamp-1">
+              {user.name}
+            </p>
+            <p className="small-regular text-light-3 text-center line-clamp-1">
+              @{user.username}
+            </p>
+          </div>
+
+          <Button
+            type="button"
+            size="sm"
+            className="shad-button_primary px-5"
+          >
+            Follow
+          </Button>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default UserCard;
